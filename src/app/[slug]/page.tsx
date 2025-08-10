6c0d647602f9e71e76ede073079d272420d31350
Praba Hridayami <prabahridayami97@gmail.com>
2025-08-10 23:20:53 +0800

    classic theme static

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,9 +1,15 @@
-import clientPromise from "@/lib/mongodb";
-import { GetServerSideProps } from "next";
 import React from "react";
-import { getPosts } from "../api/rsvp/route";
 import { getEventBySlug } from "../api/event/route";
 import RsvpForm from "@/components/forms/RsvpForm";
+import Image from "next/image";
+import { Tangerine } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const tangerin = Tangerine({
+  variable: "--font-tangerine",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 type Props = {
   params: { slug: string };
@@ -12,20 +18,117 @@ type Props = {
 
 const Page = async ({ params, searchParams }: Props) => {
   const { slug } = await params;
-  const guest =
-    typeof searchParams.guest === "string" ? searchParams.guest : "";
+  const { guest } = await searchParams;
 
   const event = slug && (await getEventBySlug(slug));
 
-  console.log("Event:", event);
-
-  if (!event) {
+  if (!event && !guest) {
     return <div>Event not found</div>;
   }
 
   return (
-    <div>
-      <RsvpForm eventId={event._id} guest={guest} />
+    <div className="bg-[#fdfcf7]">
+      <div className={cn("container relative mx-auto p-4 xl:w-1/3")}>
+        {/* Background frames */}
+        <div className="absolute mb-8 top-0 left-0 right-0 h-[300px] lg:h-[500px] z-0 pointer-events-none">
+          <Image src="/assets/top-frame.png" alt="top-frame" fill priority />
+        </div>
+
+        <div className="absolute mb-8 bottom-0 left-0 right-0 h-[300px] lg:h-[500px] z-0 pointer-events-none">
+          <Image
+            src="/assets/bottom-frame.png"
+            alt="bottom-frame"
+            fill
+            priority
+          />
+        </div>
+
+        {/* Main content */}
+        <div className="relative z-10 bg-transparent p-8 py-20 lg:p-20 xl:p-30 text-center space-y-10">
+          <div className="space-y-4">
+            <h1 className="text-2xl font-bold">Dear,&nbsp;{guest}</h1>
+            <h2 className="text-lg lg:text-2xl">
+              You&apos;re Invited to the Wedding of
+            </h2>
+          </div>
+
+          <div
+            className={`${tangerin.className} relative text-5xl lg:text-7xl font-bold mb-4`}
+          >
+            <h2 className="absolute top-0 right-0 left-0 text-[#0d3316] font-bold">
+              Alex Soto
+            </h2>
+            <h2 className="text-[#e4d8be] opacity-50 text-[120px] lg:text-[200px]">
+              &
+            </h2>
+            <h2 className="absolute bottom-0 right-0 left-0 text-[#0d3316] font-bold">
+              Prema Vipassani
+            </h2>
+          </div>
+
+          <p>
+            With joyful hearts, we invite you to celebrate our union as we begin
+            this new chapter together.
+          </p>
+
+          <hr />
+
+          <div>
+            <p className="font-bold text-xl">Wedding Ceremony</p>
+            <p>📍 Edinburgh City Chambers</p>
+            <p>253 High Street, Edinburgh EH1 1YJ</p>
+            <p>🗓️ Saturday, 25th October 2025</p>
+            <p>🕤 9:30 AM – 10:30 AM</p>
+          </div>
+
+          <div>
+            <p className="font-bold text-xl">Wedding Reception</p>
+            <p>📍 Hotel Du Vin</p>
+            <p>11 Bristo Place, Edinburgh EH1 1EZ</p>
+            <p>🕤 11:00 AM – 7:00 PM</p>
+          </div>
+
+          <hr />
+
+          <p>
+            Your presence means the world to us, and is the greatest <br />
+            wedding gift we could ever ask for.
+          </p>
+          <p>
+            However, if giving is your way of showing love, a contribution
+            towards Prema’s visa application would be received with heartfelt
+            gratitude and deeply appreciated as we take this journey together as
+            husband and wife.
+          </p>
+
+          <div>
+            <p>With love,</p>
+            <p className={cn(tangerin.className, "text-4xl")}>Alex & Prema</p>
+          </div>
+
+          <div>
+            <p className="mb-3">Our Joint Bank Account Details</p>
+            <p>Account Name: P Vipassani & A Soto Lopez</p>
+            <p>Sort Code: 23-01-20</p>
+            <p>Account Number: 14167362</p>
+            <p>Bank: Revolut</p>
+          </div>
+
+          <p>Kindly RSVP before 30th September 2025</p>
+
+          <RsvpForm eventId={event._id} guest={guest} />
+
+          <div className="relative w-full h-[573px] max-w-[430px] max-h-[573px] mt-8 text-center mx-auto">
+            <Image
+              src="/assets/alex_and_prema.webp"
+              alt="alex_prema"
+              fill
+              style={{ objectFit: "cover" }}
+              sizes="100vw"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/forms/RsvpForm.tsx
+++ src/components/forms/RsvpForm.tsx
@@ -1,67 +1,131 @@
 "use client";
-import React, { useState } from "react";
+
+import React from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 
 type Props = {
   eventId: string;
-  guest?: string;
+  guest: string | string[] | undefined;
 };
 
-const RsvpForm = ({ eventId, guest }: Props) => {
-  const [isAttending, setIsAttending] = useState<boolean>(false);
-  const [numberOfCompanion, setNumberOfCompanion] = useState<number>(0);
+const FormSchema = z
+  .object({
+    attending: z.enum(["yes", "no"], {
+      error: "Please confirm your attendance",
+    }),
+    companion: z.string().min(1, {
+      message: "Companion is required",
+    }),
+  })
+  .refine((data) => data.attending === "yes" || data.companion === "", {
+    message: "If you are attending, you must provide a companion.",
+  });
 
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
+const RsvpForm = ({ eventId, guest }: Props) => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit = async (data: z.infer<typeof FormSchema>) => {
+    // event.preventDefault();
     // Handle form submission logic here
 
     console.log("Event ID:", eventId);
     console.log("Guest:", guest);
-    console.log("Form submitted", isAttending, numberOfCompanion);
+    // console.log("Form submitted", isAttending, numberOfCompanion);
 
-    const res = await fetch("/api/rsvp", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        eventId,
-        guest,
-        isAttending,
-        numberOfCompanion,
-      }),
-    });
+    // const res = await fetch("/api/rsvp", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify({
+    //     eventId,
+    //     guest,
+    //     isAttending,
+    //     numberOfCompanion,
+    //   }),
+    // });
 
-    const data = await res.json();
+    // const data = await res.json();
     console.log(data);
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="attending">Will you attend?</label>
-        <select
-          id="attending"
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-10">
+        <FormField
+          control={form.control}
           name="attending"
-          required
-          onChange={(e) => setIsAttending(e.target.value === "yes")}
-        >
-          <option value="">Select</option>
-          <option value="yes">Yes</option>
-          <option value="no">No</option>
-        </select>
-
-        <label htmlFor="companion">Number of companion:</label>
-        <input
-          type="number"
-          id="companion"
-          name="companion"
-          required
-          onChange={(e) =>
-            setNumberOfCompanion(e.target.value ? parseInt(e.target.value) : 0)
-          }
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>Confirm your attendance:</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex flex-col gap-4"
+                >
+                  <FormItem className="flex items-center gap-3">
+                    <FormControl>
+                      <RadioGroupItem value="yes" />
+                    </FormControl>
+                    <FormLabel>Yes, I&apos;ll be there</FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center gap-3">
+                    <FormControl>
+                      <RadioGroupItem value="no" />
+                    </FormControl>
+                    <FormLabel>No, but I wish you all the best!</FormLabel>
+                  </FormItem>
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
 
-        <button type="submit">Submit</button>
+        <FormField
+          control={form.control}
+          name="companion"
+          render={({ field }) => (
+            <FormItem className="text-left">
+              <FormLabel>Number of companions:</FormLabel>
+              <FormControl>
+                <Input
+                  type="number"
+                  placeholder="Enter number of companions"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button
+          type="submit"
+          onClick={() => {
+            console.log("test");
+          }}
+        >
+          Submit
+        </Button>
       </form>
-    </div>
+    </Form>
   );
 };
 
